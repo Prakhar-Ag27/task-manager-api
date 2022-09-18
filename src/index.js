de5c997f99d5a230758b77a835e62e41fc9fed0a
unknown <agrawal.prakhar35@gmail.com>
2022-09-18 21:11:06 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+require('./db/mongoose.js')
+const userRouter = require('./routers/user.js')
+const taskRouter = require('./routers/task.js')
+
+
+const app = express()
+const port = process.env.PORT
+
+app.use((req, res, next) => {
+    console.log(req.method, req.path)
+    next()
+})
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server started at port '+ port + '!')
+})
